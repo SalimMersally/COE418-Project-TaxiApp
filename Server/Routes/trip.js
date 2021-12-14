c724d4304d048dc278b5827b0cf22fe1108d6ff5
Lamis Ar <lamis.armoush@hotmail.com>
2021-12-14 22:47:36 +0200

    "fix trip"

--- Server/Routes/trip.js
+++ Server/Routes/trip.js
@@ -2,8 +2,26 @@ module.exports = {
   getAllCurrentTrips: (req, res, db) => {
     const clientID = req.query.clientID;
     const sqlSelect =
-      "SELECT c.name as company,fromloc.city as FCity,fromloc.building as FBuilding, fromloc.street as FStreet,toloc.city as tCity, toloc.building as tBuilding,toloc.street as tStreet,t.Time,t.Date,t.nbOfSeat,t.Description, t.tripID,t.numberOfPackages,car.manifacturerCompany,d.firstName,d.lastName,car.model, car.color,car.licenseChar, car.licenseNB,d.mobileNB as DNumber, c.mobileNB as CNumber FROM trip t JOIN location fromloc JOIN location toloc JOIN car JOIN driver d JOIN company C ON fromloc.locationID = t.fromLocationID AND toloc.locationID = t.toLocationID AND t.licenseChar = car.licenseChar AND t.licenseNB = car.licenseNB AND t.drivingLicenseNB = d.drivingLicenseNB AND c.companyID = t.companyID WHERE DATE > current_date() AND clientID = ? ";
+      "SELECT  LF., TF. ,T.time, T.date, T.numberOfPackages, T.nbOfSeat, C.name FROM location LF, trip T , location TF , COMPANY C WHERE  LF.locationID = T.fromLocationID AND T.toLocationID = TF.locationID AND C.companyID = T.companyID AND clientID = ?;";
     db.query(sqlSelect, [clientID], function (err, result) {
+      console.log(err);
+      res.send(result);
+    });
+  },
+  getDriverForTrip: (req, res, db) => {
+    const tripID = req.query.tripID;
+    const sqlSelect =
+      "SELECT  firstName, lastName, mobileNB, drivingLicenseNB FROM  TRIP NATURAL JOIN DRIVER WHERE tripID = ? ";
+    db.query(sqlSelect, [tripID], function (err, result) {
+      console.log(err);
+      res.send(result);
+    });
+  },
+  getCarForTrip: (req, res, db) => {
+    const tripID = req.query.tripID;
+    const sqlSelect =
+      "SELECT  licenseChar, licenseNB, model, color, manifacturerCompany FROM  TRIP NATURAL JOIN CAR WHERE tripID = ? ";
+    db.query(sqlSelect, [tripID], function (err, result) {
       console.log(err);
       res.send(result);
     });
